03a03d7b1d20cd908393b6fee82eb463fdfc1be7
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2021-11-04 15:25:16 +0000

    code comments

--- Barcode Scanner/Form1.cs
+++ Barcode Scanner/Form1.cs
@@ -25,17 +25,19 @@ namespace Barcode_Scanner
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // create an object to choose image extensions of jpg and png
             using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "JPG|*.jpg|PNG|*.png" })
             {
+                // if an image has been selected then
                 if(openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
-                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    pictureBox1.Image = Image.FromFile(openFileDialog.FileName); // put the image in the picture box on screen
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize; // change the size of the picturebox to auto
                     BarcodeResult result = BarcodeReader.QuicklyReadOneBarcode(pictureBox1.Image, BarcodeEncoding.QRCode
                         | BarcodeEncoding.Code128, true);
 
                     if (result != null)
-                        textBox1.Text = result.ToString();
+                        textBox1.Text = result.ToString(); // put the result of the barcode in the textbox
                 }
             }
         }
