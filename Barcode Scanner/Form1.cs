579a7c951a77e1b97dae95bb0ce9798897e5e64b
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2021-11-04 15:23:14 +0000

    Add project files.

---
+++ Barcode Scanner/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IronBarCode;
+
+namespace Barcode_Scanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "JPG|*.jpg|PNG|*.png" })
+            {
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    BarcodeResult result = BarcodeReader.QuicklyReadOneBarcode(pictureBox1.Image, BarcodeEncoding.QRCode
+                        | BarcodeEncoding.Code128, true);
+
+                    if (result != null)
+                        textBox1.Text = result.ToString();
+                }
+            }
+        }
+    }
+}
